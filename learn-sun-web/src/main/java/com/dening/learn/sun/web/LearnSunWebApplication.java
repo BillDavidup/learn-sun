553f82df907e1ed6481f71ff5ad30d7d1ad5e162
wudening <wudening@meishubao.com>
2021-04-20 21:36:32 +0800

    项目搭建：spring boot 2 多模块项目搭建

---
+++ learn-sun-web/src/main/java/com/dening/learn/sun/web/LearnSunWebApplication.java
@@ -0,0 +1,13 @@
+package com.dening.learn.sun.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSunWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSunWebApplication.class, args);
+    }
+
+}
---
+++ learn-sun-web/src/test/java/com/dening/learn/sun/web/LearnSunWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dening.learn.sun.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSunWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
