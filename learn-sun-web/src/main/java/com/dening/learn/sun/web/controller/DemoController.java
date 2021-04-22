e0de60be46407e400bdb6f86d59c2a33dc524ddc
wudening <wudening@meishubao.com>
2021-04-22 17:53:14 +0800

    Controller：MVC请求方式及接收参数Demo

--- learn-sun-web/src/main/java/com/dening/learn/sun/web/controller/DemoController.java
+++ learn-sun-web/src/main/java/com/dening/learn/sun/web/controller/DemoController.java
@@ -1,9 +1,17 @@
 package com.dening.learn.sun.web.controller;
 
+import com.dening.learn.sun.web.param.DemoObjParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Author: wudening
@@ -14,9 +22,96 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/demo")
 public class DemoController {
 
+    /**
+     * mvc:get方法
+     * @param name
+     * @return
+     */
     @GetMapping("/get/example")
     public String getDemoMethod(@RequestParam String name) {
         return "Hello " + name + ",Welcome to demo controller";
+    }
+
+    /**
+     * mvc:post方法
+     * @param userId
+     * @param userName
+     * @return
+     */
+    @PostMapping("/post/example")
+    public String postDemoMethod(@RequestParam(required = false, name = "uid", value = "uid") Long userId, @RequestParam String userName) {
+        String helloMessage = "";
+        if (!Objects.isNull(userId)) {
+            helloMessage = "Hello " + userId + " " + userName + " ,Welcome to demo controller!";
+        } else {
+            helloMessage = "Hello " + userName + " ,Welcome to demo controller!";
+        }
+        return helloMessage;
+    }
+
+    /**
+     * post:参数用对象来接收
+     * @param demoObjParam
+     * @return
+     */
+    @PostMapping("/post/example/obj")
+    public String postDemoMethodObj(@RequestBody DemoObjParam demoObjParam) {
+        String helloMessage = demoObjParam.getUserId() + " " + demoObjParam.getUserName() + ", Welcome to demoObjParam";
+        return helloMessage;
+    }
+
+    /**
+     * post:参数用List列表对象来接收
+     * @param objList
+     * @return
+     */
+    @PostMapping("/post/example/obj/list")
+    public String postDemoMethodObjList(@RequestBody List<DemoObjParam> objList) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello ");
+        for (DemoObjParam demoObjParam : objList) {
+            sb.append("(").append(demoObjParam.getUserId()).append(" ").append(demoObjParam.getUserName()).append(")").append(",");
+        }
+        sb.append("Welcome to obj list demo param");
+        String helloMessage = sb.toString();
+        return helloMessage;
+    }
+
+    /**
+     * post:参数用Map来接收
+     * @param map
+     * @return
+     */
+    @PostMapping("/post/example/obj/map")
+    public String postDemoMethodObjMap(@RequestBody Map<String, Object> map) {
+        String s = "";
+        String userId = "";
+        String userName = "";
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String key = entry.getKey();
+            if ("userId".equals(key)) {
+                userId = entry.getValue().toString();
+            }
+            if ("userName".equals(key)) {
+                userName = entry.getValue().toString();
+            }
+        }
+        s = "(" + userName + " " + userId + ")" + " Welcome to obj list demo param";
+        return s;
+    }
+
+    /**
+     * post:参数从header请求头中获取
+     * @param request
+     * @param name
+     * @return
+     */
+    @PostMapping("/post/example/header/param")
+    public String postDemoMethodHeader(HttpServletRequest request, String name) {
+        String uid = request.getHeader("uid");
+        String s = "(" + name + " " + uid + ")" + ",Welcome to HttpServletRequest demo";
+        System.out.println(request.toString());
+        return s;
     }
 
 }
---
+++ learn-sun-web/src/main/java/com/dening/learn/sun/web/param/DemoObjParam.java
@@ -0,0 +1,14 @@
+package com.dening.learn.sun.web.param;
+
+import lombok.Data;
+
+/**
+ * @Author: wudening
+ * @Description: API层对象类型参数
+ * @Date: 2021/4/22 2:28 下午
+ */
+@Data
+public class DemoObjParam {
+    Long userId;
+    String userName;
+}
